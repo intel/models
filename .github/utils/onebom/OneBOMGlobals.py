c070788057e205d79fff864c0a22f89308e84808
Clayne Robison <clayne.b.robison@intel.com>
2024-09-24 15:48:52 -0700

    add support for IPX (#2415)

--- .github/utils/onebom/OneBOMGlobals.py
+++ .github/utils/onebom/OneBOMGlobals.py
@@ -1,8 +1,14 @@
 
-AIRM_PREFIX = "Intel® AI Reference Models -"
+AIRM_SPEED_PREFIX = "Intel(R) AI Reference Models -"
+AIRM_IPX_PREFIX = "Intel(R)AIReferenceModels"
+AIRM_IP_LIBRARY = "frameworks_ai"
+AIRM_REQUIREMENTS_FILE = "3.2_requirements.txt.files.txt"
 ROOT_STR_LEN = "./".__len__()
 REQUIREMENTS_STR_LEN = "/requirements.txt".__len__()
 DEBUG=False
+
+def info(msg):
+  print (f"INFO: {msg}")
 
 def debug (msg):
   if DEBUG:
